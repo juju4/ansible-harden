40c265bc594a04a02e9782188e8fa563e82ceb45
juju4 <juju4@users.noreply.github.com>
2016-10-10 21:40:00 -0400

    sync repositories

--- test/integration/default/serverspec/packages_spec.rb
+++ test/integration/default/serverspec/packages_spec.rb
@@ -3,9 +3,11 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-#for p in rkhunter acct sysstat acl
-#    describe package(#{p}), :if => os[:family] == 'ubuntu' || os[:family] == 'debian'  do
-#      it { should be_installed }
-#    end
-#end
+pkg_list = [ 'rkhunter', 'acct', 'sysstat', 'acl' ]
+
+for p in pkg_list do
+    describe package("#{p}"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian'  do
+      it { should be_installed }
+    end
+end
 
