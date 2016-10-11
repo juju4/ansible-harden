b24bca9a58400f8b6de3875ce4fe492bf3c199d5
juju4 <juju4@users.noreply.github.com>
2016-10-10 23:34:43 -0400

    galaxy naming for dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,8 +5,8 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-[ ! -d $rolesdir/ipv6 ] && git clone https://github.com/juju4/ansible-ipv6 $rolesdir/ipv6
-[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/redhat-epel
-[ ! -d $rolesdir/harden-sysctl ] && git clone https://github.com/juju4/ansible-harden-sysctl $rolesdir/harden-sysctl
+[ ! -d $rolesdir/juju4.ipv6 ] && git clone https://github.com/juju4/ansible-ipv6 $rolesdir/juju4.ipv6
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.harden-sysctl ] && git clone https://github.com/juju4/ansible-harden-sysctl $rolesdir/juju4.harden-sysctl
 [ ! -d $rolesdir/kbrebanov.osquery ] && git clone https://github.com/kbrebanov/ansible-osquery.git $rolesdir/kbrebanov.osquery
 
