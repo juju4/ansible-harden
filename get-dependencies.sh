0194c5fc283935b4fc6bd7de49f4e175a12e8b04
juju4 <juju4@users.noreply.github.com>
2018-08-27 06:02:17 -0400

    full suite test: dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,6 +8,7 @@ rolesdir=$(dirname $0)/..
 [ ! -d $rolesdir/juju4.ipv6 ] && git clone https://github.com/juju4/ansible-ipv6 $rolesdir/juju4.ipv6
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.harden-sysctl ] && git clone https://github.com/juju4/ansible-harden-sysctl $rolesdir/juju4.harden-sysctl
+[ ! -d $rolesdir/juju4.harden-mailserver ] && git clone https://github.com/juju4/ansible-harden-mailserver $rolesdir/juju4.harden-mailserver
 [ ! -d $rolesdir/kbrebanov.osquery ] && git clone https://github.com/kbrebanov/ansible-osquery.git $rolesdir/kbrebanov.osquery
 [ ! -d $rolesdir/juju4.auditd ] && git clone https://github.com/juju4/ansible-auditd $rolesdir/juju4.auditd
 ## galaxy naming: kitchen fails to transfer symlink folder
@@ -16,4 +17,3 @@ rolesdir=$(dirname $0)/..
 
 ## don't stop build on this script return code
 true
-
