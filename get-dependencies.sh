5f184cc7ebb37398370513cbaaf637f58dc5896d
juju4 <juju4@users.noreply.github.com>
2018-08-27 19:31:18 -0400

    full suite test: dependency osquery, custom version

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,8 @@ rolesdir=$(dirname $0)/..
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.harden-sysctl ] && git clone https://github.com/juju4/ansible-harden-sysctl $rolesdir/juju4.harden-sysctl
 [ ! -d $rolesdir/juju4.harden-mailserver ] && git clone https://github.com/juju4/ansible-harden-mailserver $rolesdir/juju4.harden-mailserver
-[ ! -d $rolesdir/kbrebanov.osquery ] && git clone https://github.com/kbrebanov/ansible-osquery.git $rolesdir/kbrebanov.osquery
+#[ ! -d $rolesdir/kbrebanov.osquery ] && git clone https://github.com/kbrebanov/ansible-osquery.git $rolesdir/kbrebanov.osquery
+[ ! -d $rolesdir/kbrebanov.osquery ] && git clone https://github.com/juju4/ansible-osquery.git $rolesdir/kbrebanov.osquery
 [ ! -d $rolesdir/juju4.auditd ] && git clone https://github.com/juju4/ansible-auditd $rolesdir/juju4.auditd
 [ ! -d $rolesdir/juju4.syslogclient ] && git clone https://github.com/juju4/ansible-syslogclient $rolesdir/juju4.syslogclient
 ## galaxy naming: kitchen fails to transfer symlink folder
