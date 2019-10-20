8ad3a19ff4db14560aba82e8457acc698dfe0cda
juju4 <juju4@users.noreply.github.com>
2019-10-20 09:39:28 -0400

    test/full: allow different rsyslog syntax +parenthesis

--- test/integration/full/serverspec/rsyslogd_spec.rb
+++ test/integration/full/serverspec/rsyslogd_spec.rb
@@ -19,6 +19,6 @@ end
 #end
 
 describe file('/etc/rsyslog.conf') do
-  it { should match /\$IncludeConfig \/etc\/rsyslog.d\/\*.conf|include\(file="\/etc\/rsyslog.d\/\*.conf" mode="optional"\)/ }
+  it { should match /(\$IncludeConfig \/etc\/rsyslog.d\/\*.conf|include\(file="\/etc\/rsyslog.d\/\*.conf" mode="optional"\))/ }
 end
 
