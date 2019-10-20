090be30ae71fd97c3f1ab520c59e63cc9422d7ef
juju4 <juju4@users.noreply.github.com>
2019-10-20 11:33:34 -0400

    test/full/monit: syntax, rubocop review

--- test/integration/full/serverspec/monit_spec.rb
+++ test/integration/full/serverspec/monit_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'serverspec'
 
 # Required by serverspec
@@ -20,15 +22,15 @@ end
 describe file('/etc/monit/conf.d/sshd'), :if => os[:family] == 'ubuntu' && os[:release] == '12.04' do
   it { should be_file }
 end
-describe file('/etc/monitrc'), :if => os[:family] == 'redhat' && not os[:release].match(/^8\./)  do
+describe file('/etc/monitrc'), :if => os[:family] == 'redhat' && !os[:release].match(/^8\./)  do
   it { should be_file }
 end
-describe file('/etc/monit.d/sshd'), :if => os[:family] == 'redhat' && not os[:release].match(/^8\./) do
+describe file('/etc/monit.d/sshd'), :if => os[:family] == 'redhat' && !os[:release].match(/^8\./) do
   it { should be_file }
 end
 
-describe command("monit status (#{os[:family]}, #{os[:release]})"), :if => os[:family] != 'redhat' || not os[:release].match(/^8\./) do
-  its(:stdout) { should match /monitoring status.*Monitored/ }
-  its(:stdout) { should_not match /monitoring status.*Not Monitored/ }
+describe command("monit status (#{os[:family]}, #{os[:release]})"), :if => os[:family] != 'redhat' || !os[:release].match(/^8\./) do
+  its(:stdout) { should match(/monitoring status.*Monitored/) }
+  its(:stdout) { should_not match(/monitoring status.*Not Monitored/) }
   its(:exit_status) { should eq 0 }
 end
