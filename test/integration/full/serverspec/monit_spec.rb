0fc49aeab570f35ac5c92117e4d0e90df6bb6633
juju4 <juju4@users.noreply.github.com>
2019-10-19 21:00:06 -0400

    test/full/serverspec: no monit on redhat8

--- test/integration/full/serverspec/monit_spec.rb
+++ test/integration/full/serverspec/monit_spec.rb
@@ -3,7 +3,7 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe service('monit') do
+describe service('monit'), :if => os[:family] != 'redhat' || os[:release] != '8' do
   it { should be_enabled }
   it { should be_running }
 end
@@ -20,14 +20,14 @@ end
 describe file('/etc/monit/conf.d/sshd'), :if => os[:family] == 'ubuntu' && os[:release] == '12.04' do
   it { should be_file }
 end
-describe file('/etc/monitrc'), :if => os[:family] == 'redhat' do
+describe file('/etc/monitrc'), :if => os[:family] == 'redhat' && os[:release] != '8' do
   it { should be_file }
 end
-describe file('/etc/monit.d/sshd'), :if => os[:family] == 'redhat' do
+describe file('/etc/monit.d/sshd'), :if => os[:family] == 'redhat' && os[:release] != '8' do
   it { should be_file }
 end
 
-describe command('monit status') do
+describe command('monit status'), :if => os[:family] != 'redhat' || os[:release] != '8' do
   its(:stdout) { should match /monitoring status.*Monitored/ }
   its(:stdout) { should_not match /monitoring status.*Not Monitored/ }
   its(:exit_status) { should eq 0 }
