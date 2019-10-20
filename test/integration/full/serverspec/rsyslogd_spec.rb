af2025e99a08f6378f13f8395f94f1a229cf3f02
juju4 <juju4@users.noreply.github.com>
2019-10-20 12:53:22 -0400

    test/full/rsyslog: syntax+rubocop review

--- test/integration/full/serverspec/rsyslogd_spec.rb
+++ test/integration/full/serverspec/rsyslogd_spec.rb
@@ -1,24 +1,27 @@
+# frozen_string_literal: true
+
 require 'serverspec'
 
 # Required by serverspec
 set :backend, :exec
 
-#describe service('rsyslogd'), :if => os[:family] == 'redhat' do
 describe service('rsyslog') do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe process("rsyslogd") do
+describe process('rsyslogd') do
   it { should be_running }
   its(:count) { should eq 1 }
 end
 
-#describe port(514) do
-#  it { should be_listening }
-#end
+# describe port(514) do
+#   it { should be_listening }
+# end
 
-describe file('/etc/rsyslog.conf') do
-  it { should match /(\$IncludeConfig \/etc\/rsyslog.d\/\*.conf|include\(file="\/etc\/rsyslog.d\/\*.conf" mode="optional"\))/ }
+describe file('/etc/rsyslog.conf'), :if => os[:family] != 'redhat' || !os[:release].match(/^8\./) do
+  it { should contain '$IncludeConfig /etc/rsyslog.d/*.conf' }
 end
-
+describe file('/etc/rsyslog.conf'), :if => os[:family] == 'redhat' && os[:release].match(/^8\./) do
+  it { should contain 'include(file="/etc/rsyslog.d/*.conf" mode="optional")' }
+end
