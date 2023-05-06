04b91387c073a6e9fafcd3661a65f8d5dfdef25c
juju4 <juju4@users.noreply.github.com>
2023-05-06 13:28:44 +0000

    fix: rename dependency role

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,7 +14,7 @@ fi
 [ ! -d $rolesdir/juju4.harden_sysctl ] && git clone https://github.com/juju4/ansible-harden-sysctl $rolesdir/juju4.harden_sysctl
 [ ! -d $rolesdir/juju4.harden_mailserver ] && git clone https://github.com/juju4/ansible-harden-mailserver $rolesdir/juju4.harden_mailserver
 #[ ! -d $rolesdir/kbrebanov.osquery ] && git clone https://github.com/kbrebanov/ansible-osquery.git $rolesdir/kbrebanov.osquery
-[ ! -d $rolesdir/kbrebanov.osquery ] && git clone https://github.com/juju4/ansible-osquery.git $rolesdir/kbrebanov.osquery
+[ ! -d $rolesdir/juju4.osquery ] && git clone https://github.com/juju4/ansible-osquery.git $rolesdir/juju4.osquery
 [ ! -d $rolesdir/juju4.auditd ] && git clone https://github.com/juju4/ansible-auditd $rolesdir/juju4.auditd
 [ ! -d $rolesdir/juju4.syslogclient ] && git clone https://github.com/juju4/ansible-syslogclient $rolesdir/juju4.syslogclient
 [ ! -d $rolesdir/juju4.ntpclient ] && git clone https://github.com/juju4/ansible-ntpclient $rolesdir/juju4.ntpclient
